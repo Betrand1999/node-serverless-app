9ee381d2cc852d79d3c1c5ad8dffe265ce5e5296
BETRAND <mmutagha@gmail.com>
2025-10-21 23:26:07 -0400

    add

--- backend/handler.ts
+++ backend/handler.ts
@@ -6,7 +6,7 @@ export const hello = async (
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from Lambda! Successfully deployed using Node.js runtime 22 Using API custom domain'
+      message: 'Hello from Lambda! Successfully deployed using Node.js runtime 18 Using API custom domain'
     }),
   };
 };
