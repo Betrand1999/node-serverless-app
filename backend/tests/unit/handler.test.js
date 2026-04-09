4f16ad30f70311e1d935220497d3215a7c2fde6a
BETRAND <mmutagha@gmail.com>
2026-04-09 11:03:04 -0400

    add unit test

---
+++ backend/tests/unit/handler.test.js
@@ -0,0 +1,8 @@
+// backend/tests/unit/handler.test.js
+const assert = require('assert');
+
+describe('Unit Test', () => {
+  it('adds numbers correctly', () => {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
