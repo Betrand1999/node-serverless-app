282bea704e8c8ac7be9f3b012f4571f63c7d4461
BETRAND <mmutagha@gmail.com>
2026-07-17 21:18:39 -0400

    Target EC2 self-hosted agent

--- backend/handler.ts
+++ backend/handler.ts
@@ -6,7 +6,7 @@ export const helloo = async (
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from Lambda! Successfully deployed Dev 4a love love'
+      message: 'Hello from Lambda! Successfully deployed Dev Betrand'
 
     }),
   };
